df0ac5d2147d5ad708275c1220af80acbc7bb5a8
muhammad <memomq70@gmail.com>
2024-02-20 14:31:13 +0200

    created Dockerfile for polybot and updated compose

--- docker_project/polybot/app.py
+++ docker_project/polybot/app.py
@@ -1,9 +1,8 @@
 import flask
 from flask import request
 import os
-
 from loguru import logger
-from bot import ObjectDetectionBot, Bot
+from bot import ObjectDetectionBot
 
 app = flask.Flask(__name__)
 
@@ -23,5 +22,5 @@ def webhook():
     return 'Ok'
 
 if __name__ == "__main__":
-    bot = Bot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
+    bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
     app.run(host='0.0.0.0', port=8443)
