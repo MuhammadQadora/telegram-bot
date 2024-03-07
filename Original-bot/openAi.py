c373fa4d3da5683e016ab7b84c5e720574fc9568
muhammad <memomq70@gmail.com>
2024-03-07 22:50:15 +0200

    upgraded to gpt-4 not 3

---
+++ Original-bot/openAi.py
@@ -0,0 +1,41 @@
+import os
+from openai import OpenAI
+import openai
+from loguru import logger
+from io import BytesIO
+import requests
+
+openai_key = os.environ['openai_key']
+
+class AI():
+    def __init__(self):
+        self.client = OpenAI(api_key=openai_key)
+    
+    def gpt(self,user_message):
+        try:
+            response = self.client.chat.completions.create(
+                messages=user_message,
+                model="gpt-4"
+            )
+            assistant_response = response.choices[0].message.content
+            return assistant_response
+        except openai.OpenAIError as e:
+            logger.info(e)
+
+    
+    def text_to_image(self,user_message):
+        try:
+            response = self.client.images.generate(
+                model="dall-e-3",
+                prompt=f"{user_message}",
+                size="1024x1024",
+                quality="hd",
+                n=1
+            )
+
+            image_url = response.data[0].url
+            image_data = requests.get(image_url).content
+            return image_data
+        except openai.OpenAIError as e:
+            logger.info(e)
+
