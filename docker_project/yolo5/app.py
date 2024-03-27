c6a974e19599a1b943c02b7414868a65bacd2442
muhammad <memomq70@gmail.com>
2024-03-27 14:48:29 +0200

    First commit

--- docker_project/yolo5/app.py
+++ docker_project/yolo5/app.py
@@ -1,7 +1,8 @@
 import json
 import time
+import datetime
 from pathlib import Path
-from flask import Flask, request,make_response
+import botocore
 from detect import run
 import uuid
 import yaml
@@ -9,98 +10,150 @@ from loguru import logger
 import os
 import boto3
 from botocore.exceptions import ClientError
-from mongoApi import mongoAPI
-from bson import json_util
 
-images_bucket = os.environ['BUCKET_NAME']
-mongo_user = os.environ['MONGO_USER']
-mongo_pass = os.environ['MONGO_PASS']
+from bson import json_util
+############################
+###### TODO ###############
+images_bucket = 'mqbucket1'
 database = 'images'
 collection = 'predictions'
-
+region_name='us-east-1'
+queue_url = 'telegrambot-yolo5-mf'
+sns_topic_arn = 'arn:aws:sns:us-east-1:933060838752:telegrambot-sns-mf'
 with open("data/coco128.yaml", "rb") as stream:
     names = yaml.safe_load(stream)['names']
-app = Flask(__name__)
 
-@app.route('/predict', methods=['POST','GET'])
+sqs_client = boto3.client('sqs',region_name=region_name)
+sns_client = boto3.client('sns',region_name=region_name)
+dynamodb = boto3.client('dynamodb', region_name=region_name)
+
 def predict():
-    # Generates a UUID for this current prediction HTTP request. This id can be used as a reference in logs to identify and track individual prediction requests.
-    prediction_id = str(uuid.uuid4())
+    while True:
+        response = sqs_client.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=60,
+            WaitTimeSeconds=20
+        )
+        prediction_id = str(uuid.uuid4())
 
-    logger.info(f'prediction: {prediction_id}. start processing')
-    # Receives a URL parameter representing the image to download from S3
-    img_name = request.args.get('imgName')
-    # TODO download img_name from S3, store the local image path in original_img_path
-    client = boto3.client('s3')
-    #create directory if it does not exist
-    if not os.path.exists('Images'):
-        os.mkdir('Images')
-    #download image to Images folder
-    try:
-        client.download_file(images_bucket, img_name, f"Images/{os.path.basename(img_name)}")
-    except ClientError as e:
-        logger.info(e)
-        return False
-    print("passed")
-    original_img_path = f"Images/{os.path.basename(img_name)}"
+        logger.info(f'prediction: {prediction_id}. start processing')
+        # Receives a URL parameter representing the image to download from S3
+        if 'Messages' in response:
+            message = response['Messages'][0]['Body']
+            receipt_handle = response['Messages'][0]['ReceiptHandle']
+            msg_id = response['Messages'][0]['MessageId']
+            logger.info(response)
+            # TODO download img_name from S3, store the local image path in original_img_path
+            client = boto3.client('s3')
+            #create directory if it does not exist
+            if not os.path.exists('Images'):
+                os.mkdir('Images')
+            #download image to Images folder
+            img_name = message
+            try:
+                client.download_file(images_bucket, img_name, f"Images/{os.path.basename(img_name)}")
+            except ClientError as e:
+                logger.info(e)
+                return False
+            original_img_path = f"Images/{os.path.basename(img_name)}"
 
-    logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
+            logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
-    # Predicts the objects in the image
-    run(
-        weights='yolov5s.pt',
-        data='data/coco128.yaml',
-        source=original_img_path,
-        project='static/data',
-        name=prediction_id,
-        save_txt=True
-    )
+            # Predicts the objects in the image
+            run(
+                weights='yolov5s.pt',
+                data='data/coco128.yaml',
+                source=original_img_path,
+                project='static/data',
+                name=prediction_id,
+                save_txt=True
+            )
 
-    logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
+            logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
 
-    # This is the path for the predicted image with labels
-    # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
-    ## had to change the predicted img path
-    predicted_img_path = Path(f"static/data/{prediction_id}/{original_img_path.split('/')[-1]}")
-    # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
-    # This will upload the predicted images with same file structure found locally
-    try:
-        client.upload_file(predicted_img_path,images_bucket,f"static/{prediction_id}/{os.path.basename(predicted_img_path)}")
-    except ClientError as e:
-        logger.info(e)
-        return False
-    # Parse prediction labels and create a summary
-    pred_summary_path = Path(f"static/data/{prediction_id}/labels/{os.path.basename(img_name).split('.')[0]}.txt")
-    if pred_summary_path.exists():
-        with open(pred_summary_path) as f:
-            labels = f.read().splitlines()
-            labels = [line.split(' ') for line in labels]
-            labels = [{
-                'class': names[int(l[0])],
-                'cx': float(l[1]),
-                'cy': float(l[2]),
-                'width': float(l[3]),
-                'height': float(l[4]),
-            } for l in labels]
+            # This is the path for the predicted image with labels
+            # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
+            ## had to change the predicted img path
+            predicted_img_path = Path(f"static/data/{prediction_id}/{original_img_path.split('/')[-1]}")
+            # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
+            # This will upload the predicted images with same file structure found locally
+            try:
+                client.upload_file(predicted_img_path,images_bucket,f"static/{prediction_id}/{os.path.basename(predicted_img_path)}")
+            except ClientError as e:
+                logger.info(e)
+                return False
+            # Parse prediction labels and create a summary
+            pred_summary_path = Path(f"static/data/{prediction_id}/labels/{os.path.basename(img_name).split('.')[0]}.txt")
+            if pred_summary_path.exists():
+                with open(pred_summary_path) as f:
+                    labels = f.read().splitlines()
+                    labels = [line.split(' ') for line in labels]
+                    labels = [{
+                        'class': names[int(l[0])],
+                        'cx': float(l[1]),
+                        'cy': float(l[2]),
+                        'width': float(l[3]),
+                        'height': float(l[4]),
+                    } for l in labels]
 
-        logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
+                logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
 
-        prediction_summary = {
-            'prediction_id': prediction_id,
-            'original_img_path': original_img_path,
-            'predicted_img_path': f"{predicted_img_path}",
-            'labels': labels,
-            'time': time.time()
-        }
+                prediction_summary = {
+                    'prediction_id': prediction_id,
+                    'original_img_path': original_img_path,
+                    'predicted_img_path': f"{predicted_img_path}",
+                    'labels': labels,
+                    'time': time.time()
+                }
 
-        #TODO store the prediction_summary in MongoDB
-        data = json.loads(json_util.dumps(prediction_summary))
-        conn = mongoAPI(mongo_user,mongo_pass,database,collection)
-        conn.insert_prediction(data)
+                try:
+                    # Get the current time in epoch format
+                    current_epoch = int(datetime.datetime.now().timestamp())
+                    # Add 5 minutes to the current time
+                    new_epoch = current_epoch + (5 * 60)  # 1 minutes in seconds
 
-        return prediction_summary
-    else:
-        return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
+                    # Upload prediction_summary to dynamodb
+                    response = dynamodb.put_item(
+                        TableName=queue_url,
+                        Item={
+                            '_id': {
+                                'S': msg_id
+                            },
+                            'TTL': {
+                                'N': str(new_epoch)
+                            },
+                            'text': {
+                                'S': prediction_summary
+                            }
+                        })
+                except botocore.exceptions as e:
+                    logger.info(e)
+                    return False
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000)
+                # Send sns to Telegrambot
+                result = {'job_id': msg_id,'Status_Code': 200}
+                try:
+                    response = sns_client.publish(
+                        TopicArn=sns_topic_arn,
+                        Message=json.dumps({'default': json.dumps(result)}),
+                        MessageStructure='json'
+                    )
+                    if response['MessageId']:
+                        # Delete message from Queue
+                        sqs_client.delete_message(
+                            QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+                except botocore.exceptions as e:
+                    logger.error(e)
+                    return False
+            else:
+                result = {'job_id': msg_id, 'Status_Code': 404}
+                response = sns_client.publish(
+                    TopicArn=sns_topic_arn,
+                    Message=json.dumps({'default': json.dumps(result)}),
+                    MessageStructure='json'
+                )
+        else:
+            continue
+
+predict()
