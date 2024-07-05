5d24a93fed0ede45e602db87d8cf4e50dfe173ec
muhammad <memomq70@gmail.com>
2024-07-05 18:34:25 +0300

    changed webhook to trigger on dev

--- Original-bot/bot.py
+++ Original-bot/bot.py
@@ -15,7 +15,7 @@ from local_user_DB import *
 list_members = []
 
 ##################
-token = secret_keys['TELEGRAM_TOKEN_DEV']
+token = os.environ['TELEGRAM_TOKEN']
 url = os.environ['TELEGRAM_APP_URL']
 bucket_name = secret_keys['BUCKET_NAME']
 queue_url = os.environ['SQS_URL']
