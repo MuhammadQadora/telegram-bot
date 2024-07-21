5dfc68405e7127f44d2c9421310a57f16cdf6981
Firas Naarani <89148516+FirasNaarani@users.noreply.github.com>
2024-07-21 21:50:55 +0300

    Feature/update start (#123)
    * small fixes

--- Original-bot/bot.py
+++ Original-bot/bot.py
@@ -72,7 +72,7 @@ class Bot:
         def start(msg):
             self.bot.send_message(
                 msg.chat.id,
-                f"Hi there {msg.from_user.first_name}.\nWelcome to my amazing bot!\n,To see what this Bot can do use /options .",
+                f"Hi there {msg.from_user.first_name}.\nWelcome to my amazing bot,\nZORO&BATMAN\nTo see what this Bot can do use /options .",
             )
             if not is_member_in_list_by_name(msg.chat.id):
                 add_member(msg.chat.id)
@@ -83,7 +83,6 @@ class Bot:
         @self.bot.message_handler(commands=["options"])
         def help(msg):
             if is_member_in_list_by_name(msg.chat.id):
-                # member = get_member_by_name(self.list_members, msg.chat.id)
                 member = get_member_from_dynamo(name=msg.chat.id)
                 for notification in member.notify:
                     member.notify[notification] = False
@@ -103,7 +102,8 @@ class Bot:
                 "Ask a question", callback_data="answer_question"
             )
             markup.add(gpt_4, yolov5, gpt_one_question, text_to_image)
-            self.bot.send_message(msg.chat.id, "Available Options", reply_markup=markup)
+            self.bot.send_message(
+                msg.chat.id, "Available Options", reply_markup=markup)
 
     def photo_handler(self):
         @self.bot.message_handler(content_types=["photo"])
@@ -128,12 +128,13 @@ class Bot:
                 memory.write(photo_binary)
                 memory.seek(0)
                 client = boto3.client("s3")
+                path = os.path.basename(file_info.file_path)
                 # try to upload picture to s3 bucket
                 try:
                     client.upload_fileobj(
                         memory,
                         bucket_name,
-                        f"OriginalBot/received/{os.path.basename(file_info.file_path)}",
+                        f"OriginalBot/received/{path}",
                     )
                 except botocore.exceptions.ClientError as e:
                     logger.info(e)
@@ -150,7 +151,8 @@ class Bot:
                             }
                         ),
                     )
-                    self.bot.send_message(msg.chat.id, "Sent Image for processing.....")
+                    self.bot.send_message(
+                        msg.chat.id, "Sent Image for processing.....")
                     logger.info(response)
                 except botocore.exceptions.ClientError as e:
                     logger.error(e)
@@ -184,7 +186,7 @@ class Bot:
             else:
                 self.bot.send_message(
                     msg.chat.id,
-                    "It seems you tried to upload a photo, if you want to detect objects got to /options\nand choose object detection",
+                    "It seems you tried to upload a photo, if you want to detect objects got to /options\nand choose [object detection]",
                 )
             update_member_notify(name=msg.chat.id, notify_updates=notify)
 
@@ -235,7 +237,8 @@ class Bot:
                     )
 
                 logger.error(notify)
-                update_member_notify(name=clk.message.chat.id, notify_updates=notify)
+                update_member_notify(
+                    name=clk.message.chat.id, notify_updates=notify)
 
     def text_handler(self):
         @self.bot.message_handler(content_types=["text"])
@@ -250,7 +253,8 @@ class Bot:
                 logger.info(f"Chat with GPT-4 Activated")
                 if msg.text == "/quit":
                     notify[Notify.GPT4] = False
-                    update_member_notify(name=msg.chat.id, notify_updates=notify)
+                    update_member_notify(
+                        name=msg.chat.id, notify_updates=notify)
                     self.bot.send_message(
                         msg.chat.id,
                         "Chat With GPT-4 Deactivated",
@@ -275,13 +279,15 @@ class Bot:
                 )
                 self.bot.send_message(msg.chat.id, f"{assistant_response}")
                 feed_to_dynamo_update = (
-                    dynamo_obj.convert_regular_dictionary_to_dynamodb(chat_history)
+                    dynamo_obj.convert_regular_dictionary_to_dynamodb(
+                        chat_history)
                 )
                 Item = dynamo_obj.template(msg.chat.id, feed_to_dynamo_update)
                 dynamo_obj.put_item(Item)
                 logger.info("Chat with GPT-4 Deactivated")
             elif notify.get(Notify.YOLO):
-                self.bot.send_message(msg.chat.id, "You must upload a photo not text")
+                self.bot.send_message(
+                    msg.chat.id, "You must upload a photo not text")
             elif notify.get(Notify.QUESTION):
                 logger.info("Ask a question activated")
                 user_role = [{"role": "user", "content": f"{msg.text}"}]
